606a51815e1f4bc4be5ac9fb06d56b5d729227c3
Colin Detwiller <colinsr@me.com>
2017-10-01 11:44:26 +0000

    looping in templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { SandboxComponent } from './components/sandbox/sandbox.component';
 import { Sandbox2Component } from './components/sandbox/sandbox2.component';
 import { Sandbox3Component } from './components/sandbox/sandbox3.component';
+import { Sandbox4Component } from './components/sandbox4/sandbox4.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SandboxComponent,
     Sandbox2Component,
-    Sandbox3Component
+    Sandbox3Component,
+    Sandbox4Component
   ],
   imports: [
     BrowserModule
