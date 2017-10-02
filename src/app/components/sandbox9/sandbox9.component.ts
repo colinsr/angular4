d0c3a81e933256b98893ba26001cf41dfe9da943
Colin Detwiller <colinsr@me.com>
2017-10-02 12:23:11 +0000

    adding more events

--- src/app/components/sandbox9/sandbox9.component.ts
+++ src/app/components/sandbox9/sandbox9.component.ts
@@ -4,14 +4,25 @@ import { Component } from '@angular/core';
     selector: 'sandbox9',
     template: `
     <h3>hello sandbox9</h3>
-    <button id="btn" (click)="fireEvent($event,'colin')">fire</button>
+    <button id="btn" (click)="fireEvent($event.type)">click</button>
+    <br><br>    
+    <button id="btn" (mouseover)="fireEvent($event.type)">mouse over</button>
+    <br><br>
+    <button id="btn" (mousedown)="fireEvent($event.type)">mouse down</button>
+    <br><br>
+    <button id="btn" (mouseup)="fireEvent($event.type)">mouse up</button>
+    <br><br>
+    <button id="btn" (dblclick)="fireEvent($event.type)">double click</button>
+    <br><br>
+    <button id="btn" (drag)="fireEvent($event)">drag</button>
+    <br><br>
+    <button id="btn" (dragover)="fireEvent($event.type)">drag over</button>
     `
 })
 
 export class Sandbox9Component{
 
-    fireEvent(e, name){
-        console.log("hello, ", name);
-        console.log(e.target.id);
+    fireEvent(eventType){
+        console.log(eventType);
     }
 }
