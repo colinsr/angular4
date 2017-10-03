29d9ca679cd3f68f399b57bc314fd66c320db624
Colin Detwiller <colinsr@me.com>
2017-10-03 12:35:16 +0000

    playing with ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SandboxComponent } from './components/sandbox/sandbox.component';
@@ -32,7 +33,8 @@ import { Sandbox12Component } from './components/sandbox12/sandbox12.component';
     Sandbox12Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/sandbox12/sandbox12.component.ts
+++ src/app/components/sandbox12/sandbox12.component.ts
@@ -4,10 +4,25 @@ import { Component } from '@angular/core';
     selector: 'sandbox12',
     template: `
     <h3>Sandbox 12</h3>
-    
+    <div class="container">
+        <form class="form">
+            <div class="form-group">
+                <label>Name</label>
+                <input type="text" class="form-control" [(ngModel)]="name" name="name" >
+            </div>
+            <div class="form-group">
+                <label>Age</label>
+                <input type="text" class="form-control" [(ngModel)]="age" name="age" >
+            </div>
+            <input type="submit" value="Submit" class="btn btn-submit" >
+        </form>
+        <h4>{{ name }}</h4>
+        <h4>{{ age  }}</h4>
+    </div>
     <br><hr>`
 })
 
 export class Sandbox12Component{
-    
+    name:string = "";
+    age:number = 0;
 }
